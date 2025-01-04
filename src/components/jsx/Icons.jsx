cf5d6eebccb05f9949546254c411beb2fecab60f
cedricicic <cedrickleung@gmail.com>
2025-01-04 15:47:23 -0500

    second day

--- src/components/jsx/Icons.jsx
+++ src/components/jsx/Icons.jsx
@@ -1,10 +1,17 @@
 function Icons(props) {
     return (
-      <a className="dock-button" key={props.id} href={props.link} target="_blank" rel="noopener noreferrer" aria-label={props.name}>
+      <a 
+        className="dock-button" 
+        key={props.id} 
+        href={props.link} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        aria-label={props.name}
+      >
         <img src={props.src} alt={props.name} />
+        <div className="tooltip">{props.name}</div>
       </a>
     );
   }
   
   export default Icons;
-  
