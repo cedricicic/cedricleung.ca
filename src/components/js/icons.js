c2a98357a5f417c4cfd38ad5bc82213a1b526142
cedricicic <cedrickleung@gmail.com>
2025-01-04 16:23:09 -0500

    second day

--- src/components/js/icons.js
+++ src/components/js/icons.js
@@ -1,31 +1,40 @@
-import github from "../../assets/images/github.png"
-import photo from "../../assets/images/photos.png"
-import mail from "../../assets/images/mail.png"
-import linkedin from "../../assets/images/linkedin.png"
+import github from "../../assets/images/github.png";
+import photo from "../../assets/images/photos.png";
+import mail from "../../assets/images/mail.png";
+import linkedin from "../../assets/images/linkedin.png";
+import resume from "../../assets/images/resume.png";
+import resumePdf from "../../assets/pdf/resume.pdf"
 
 export default [
   {
     id: 1,
+    name: "Resume",
+    link: resumePdf,
+    src: resume,
+  },
+
+  {
+    id: 2,
     name: "Github",
     link: "https://github.com/cedricicic",
     src: github,
   },
   {
-    id: 2,
+    id: 3,
     name: "Photos",
     link: "new html",
     src: photo,
   },
   {
-    id: 3,
+    id: 4,
     name: "Contact me",
     link: "new html",
     src: mail,
   },
   {
-    id: 4,
+    id: 5,
     name: "Linkedin",
     link: "https://www.linkedin.com/in/cedric-leung-38637029a/",
     src: linkedin,
-  }
+  },
 ];
