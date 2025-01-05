1e5b486e5be73116566e59419ec4c0dae7c8c216
cedricicic <cedrickleung@gmail.com>
2025-01-05 17:31:58 -0500

    navbar

---
+++ src/components/js/dropdowns.js
@@ -0,0 +1,37 @@
+import resumePdf from "../../assets/pdf/Resume.pdf";
+
+export default [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 3,
+    name: "Contact",
+    link: "/#contact",
+  },
+  {
+    id: 4,
+    name: "Resume",
+    link: resumePdf,
+  },
+  {
+    id: 5,
+    name: "Github",
+    link: "https://github.com/cedricicic",
+  },
+
+  {
+    id: 6,
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/cedric-leung-38637029a/",
+  },
+];
+
+
--- src/components/jsx/NavBar.jsx
+++ src/components/jsx/NavBar.jsx
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "../css/navbar.css";
 import logo from "../../assets/images/icon.png";
+import dropdownData from "../js/dropdowns.js";
 
 function Navbar() {
+  const [currentPage, setCurrentPage] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const location = window.location.href;
+    switch (true) {
+      case location.endsWith("/"):
+        setCurrentPage("Home");
+        break;
+      case location.endsWith("/about") || location.endsWith("/#about"):
+        setCurrentPage("About me");
+        break;
+      case location.endsWith("/contact") || location.endsWith("/#contact"):
+        setCurrentPage("Contact");
+        break;
+      default:
+        setCurrentPage("");
+        break;
+    }
+  }, []);
+
   const Clock = () => {
     const [time, setTime] = useState(
       new Date().toLocaleTimeString([], {
@@ -13,6 +35,7 @@ function Navbar() {
         month: "short",
       })
     );
+
     useEffect(() => {
       const timer = setInterval(() => {
         setTime(
@@ -26,30 +49,45 @@ function Navbar() {
         );
       }, 1000);
 
-      return () => clearInterval(timer); 
+      return () => clearInterval(timer);
     }, []);
 
     return <div className="navbar-clock">{time}</div>;
   };
 
-  useEffect(() => {
-    console.log("Current URL:", window.location.href);
-  }, []); 
-
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <div className="navbar-icon">
-            <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
-        <a href="#home" className="navbar-home-link">
-          Home
-        </a>
-        <a href="#go" className="navbar-link">
-          Go
-        </a>
+        <a className="navbar-current">{currentPage}</a>
+        <div
+          className="navbar-dropdown"
+          onMouseEnter={() => setIsDropdownOpen(true)}
+          onMouseLeave={() => setIsDropdownOpen(false)}
+        >
+          <a className="navbar-link">Go</a>
+          <ul className={`dropdown-menu ${isDropdownOpen ? "open" : ""}`}>
+            {dropdownData.map((item) => (
+              <li key={item.id}>
+                <a
+                  href={item.link}
+                  target={item.link.startsWith("http") ? "_blank" : "_self"}
+                  rel={
+                    item.link.startsWith("http")
+                      ? "noopener noreferrer"
+                      : undefined
+                  }
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <Clock /> 
+      <Clock />
     </nav>
   );
 }
