de9cc91b8e793a0a515b42194589af372d94e48d
cedricicic <cedrickleung@gmail.com>
2025-01-04 20:56:12 -0500

    second day

--- src/components/jsx/Contact.jsx
+++ src/components/jsx/Contact.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "../css/contact.css";
 
-const ContactForm = () => {
+function ContactForm() {
   return (
     <div className="contact-page">
-      <div className="contact-form">
+      <div className="contact-container">
         <h1>Contact Me</h1>
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Your email" />
-        <textarea placeholder="Your message"></textarea>
-        <input type="submit" value="Submit" />
+        <form className="contact-form">
+          <input type="text" className="form-input" placeholder="Your name" />
+          <input type="email" className="form-input" placeholder="Your email" />
+          <textarea className="form-textarea" placeholder="Your message"></textarea>
+          <button type="submit" className="form-submit">Submit</button>
+        </form>
       </div>
     </div>
   );
-};
+}
 
 export default ContactForm;
