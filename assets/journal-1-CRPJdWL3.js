40a118e272e6b551486a14c3fc7cac0b1258f598
cedricicic <cedricicic@users.noreply.github.com>
2025-01-20 00:50:49 +0000

    deploy: 9207e3ee206ad1f708d152ab1eae6be3dc5947bf

--- assets/journal-1-CRPJdWL3.js
+++
@@ -1,8 +0,0 @@
-const n=`---
-title: "WIP"
-image: "src/assets/images/ellie2.png"
-date: "WIP"
-description: "WIP"
----
-
-`;export{n as default};
---
+++ assets/journal-2-g7FWpPkZ.js
@@ -0,0 +1,8 @@
+const n=`---
+title: "HELLLLLLLO"
+image: "./src/assets/images/ellie.png"
+date: "N/A"
+description: "N/A"
+---
+
+N/A`;export{n as default};
